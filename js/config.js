9ca576591f91366772d8701702c8443d12f1b20f
Overhand-Industries <officialohi@gmail.com>
2025-02-15 13:09:24 -0800

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,5 @@
+// js/config.js
+export const LEVEL_WIDTH = 1920;
+export const GROUND_EXTRA_HEIGHT = 100;
+export const VIEWPORT_MARGIN = 100;
+// … add other constants (e.g. jumpStrength, throwSpeed, etc.) as needed.
